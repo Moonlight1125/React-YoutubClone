4422a0022e622615e2c0bb1ab4422bf57717c193
kentaro <k.suzuki1125@outlook.jp>
2024-09-11 21:00:08 +0900

    ✨ React-Router&UseContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Recommend from "./components/Recommend";
+import Sivdevar from "./components/Sivdevar";
+import Video from "./components/Video";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Sivdevar/>
+      <BrowserRouter>
+        <Header/>
+      <Routes>
+        <Route path="/" element={<Recommend/>}/>
+        <Route path="/video/:id" element={<Video/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
